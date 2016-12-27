1122331a76288a7798a290f358408804ddf98cd9
mountainman90 <ruiminzhuzhrm@gmail.com>
2016-12-27 11:25:20 -0600

    git status

--- greedy.py
+++ greedy.py
@@ -26,38 +26,37 @@ class Huffman(object):
 		self.code_symb = defaultdict(str) # dict mapping code to symbol
 		self.symb_f = defaultdict(int)
 
-	def symb_frequency(self, txt):
-		'''
-		find the frequencies of symbols in txt.
-		'''
-		for symb in txt:
-			self.symb_f[symb] += 1
-
 	def show_coding(self):
 		print('Symbol\tFrequency\tCode')
 		for symb in self.symb_code:
 			print("%s\t%s\t%s" % (symb, self.symb_f[symb], self.symb_code[symb]))
 
 	def encode(self, txt):
-		self.symb_frequency(txt)
+		for symb in txt:
+			self.symb_f[symb] += 1
 
 		pq = BinaryHeap()
 		for symb, f in self.symb_f.items():
+			#print('f:%s' %f)
 			pq.insert(f, [symb])
 
 		while pq.length() > 1:
 			m1 = pq.del_min()
 			m2 = pq.del_min()
 
+			print(m1.key, m2.key)
+
 			for symb in m1.value:
 				self.symb_code[symb] = '0' + self.symb_code[symb]
 			for symb in m2.value:
 				self.symb_code[symb] = '1' + self.symb_code[symb]
 
-			hi = pq.insert(m1.key+m2.key, m1.value+m2.value)
+			k = m1.key + m2.key
+			val = m1.value + m2.value
+			pq.insert(k, val)
 
-		for symb in self.symb_code:
-			self.code_symb[self.symb_code[symb]] = symb
+		for symb, code in self.symb_code.items():
+			self.code_symb[code] = symb
 
 	def decode(self, txt):
 		pass
