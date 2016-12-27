8b5b779cc0631c4fedcc2b440042f3f77553f6d4
mountainman90 <ruiminzhuzhrm@gmail.com>
2016-12-26 21:57:32 -0600

    Trying to fix the bug in Huffman encoding

--- greedy.py
+++ greedy.py
@@ -1,5 +1,5 @@
 from heaps import HeapItem, BinaryHeap
-
+from collections import defaultdict
 
 class Huffman(object):
 	'''
@@ -22,6 +22,47 @@ class Huffman(object):
 	credit: https://rosettacode.org/wiki/Huffman_coding#Python
 	'''
 	def __init__(self):
+		self.symb_code = defaultdict(str) # dict mapping symbol to code
+		self.code_symb = defaultdict(str) # dict mapping code to symbol
+		self.symb_f = defaultdict(int)
+
+	def symb_frequency(self, txt):
+		'''
+		find the frequencies of symbols in txt.
+		'''
+		for symb in txt:
+			self.symb_f[symb] += 1
+
+	def show_coding(self):
+		print('Symbol\tFrequency\tCode')
+		for symb in self.symb_code:
+			print("%s\t%s\t%s" % (symb, self.symb_f[symb], self.symb_code[symb]))
+
+	def encode(self, txt):
+		self.symb_frequency(txt)
+
+		pq = BinaryHeap()
+		for symb, f in self.symb_f.items():
+			pq.insert(f, [symb])
+
+		while pq.length() > 1:
+			m1 = pq.del_min()
+			m2 = pq.del_min()
+
+			for symb in m1.value:
+				self.symb_code[symb] = '0' + self.symb_code[symb]
+			for symb in m2.value:
+				self.symb_code[symb] = '1' + self.symb_code[symb]
+
+			hi = pq.insert(m1.key+m2.key, m1.value+m2.value)
+
+		for symb in self.symb_code:
+			self.code_symb[self.symb_code[symb]] = symb
+
+	def decode(self, txt):
 		pass
+
+
+
 
 
